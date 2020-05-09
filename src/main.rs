02e618bed43eab820b83eca762178fde06ac7929
bryan huizi <bryan.huizi@magnet360.com>
2020-05-09 14:32:41 -0700

    integers/float/bool/string

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 fn main() {
-    let mut x=10;
 
-    x=x+10;
+    // Unsigned integer can hold only positive value.print.
+    //Signed integer can hold both positive and negative value.
 
-    println!("value of x:{}", x);
+    // let a=10;
+    // let _b:i64 = 10;
+    // let c:u32 = 15;
 
+    // println!("value of a variable {}", a);
+    // println!("value of _b variable {}", _b);
+    // println!("value of c variable {}", c);
+
+    // Floating point
+    // let a=3.14;
+    // let _b:f32 = 2.14;
+    // let _c:f64 = 5.21;
+
+    // println!("value of a {}", a);
+    // println!("value of _b {}", _b);
+    // println!("value of _c {}", _c);
+
+    // Boolean
+    // let a = true;
+    // let b:bool = false;
+
+    // println!("value of a {}", a);
+    // println!("value of b {}", b);
+
+    // Character/String
+
+    let a='A';
+    let b="Hello";
+
+    println!("value of a {}", a);
+    println!("value of b {}", b);
 }
