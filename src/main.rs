dad8bd8178b474558ad1e3899b9ded56388e95c2
bryan huizi <bryan.huizi@magnet360.com>
2020-05-15 15:48:14 -0700

    while loop

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 
 fn main() {
 
-    // for i in 0..5 {
-    //     println!("value of is is: {}", i);
-    // }
+    let mut n = 1;
 
-    // Print 5 rows and 10 stars
-
-    // for row in 0..5 {
-    //     for stars in 0..10 {
-    //         print!("*");
-    //     }
-    //     println!();
-    // }
-
-    for row in 0..5 {
-        for stars in 0..row + 1 {
-            print!("*");
+    while n <= 10 {
+        if n % 2 == 0 {
+            println!("even number is: {}", n);
         }
-        println!();
+        else {
+            println!("odd number is: {}", n);
+        }
+        n = n + 1;
     }
 }
