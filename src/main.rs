f6ba49e26b5d1000daefce1d7f6498a67e43beeb
bryan huizi <bryan.huizi@magnet360.com>
2020-05-16 14:32:05 -0700

    if let

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-fn divide(numerator: f64, denominator: f64)->Option<f64> {
-    if denominator == 0.0 {
-        None
-    }
-    else {
-        Some(numerator/denominator)
-    }
-}
-
 fn main() {
 
-    // let result = divide(3.0, 2.0);
-    let result = divide(3.0, 1.0);
+    // let result:Option<i32>=Some(10);
+    let result:Option<i32>=None;
 
-    // println!("Result is {:?}", result);
-
-    match result {
-        Some(x) => println!("value is: {}", x),
-        None => println!("Cannot divide by 0"),
+    if let Some(i) = result {
+        println!("matched {:?}", i);
+    }
+    else {
+        println!("not matched");
     }
 }
