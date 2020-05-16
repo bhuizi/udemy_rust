a7d221b81d55108bd328a06809791bd288fa7bdd
bryan huizi <bryan.huizi@magnet360.com>
2020-05-16 14:45:00 -0700

    while let

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 fn main() {
 
-    // let result:Option<i32>=Some(10);
-    let result:Option<i32>=None;
+    let mut result:Option<i32> = Some(0);
 
-    if let Some(i) = result {
-        println!("matched {:?}", i);
+    while let Some(i) = result {
+        if i > 9 {
+            println!("value is greater then 9");
+            result = None;
+        }
+        else {
+            println!("value is less then 9");
+            result = Some(i + 1);
+        }
+
     }
-    else {
-        println!("not matched");
-    }
+    // loop {
+    //     match result {
+    //         Some(i) => {
+    //             if i > 9 {
+    //                 println!("value is greater then 9");
+    //                 result = None;
+    //             }
+    //             else {
+    //                 println!("value is less then 9");
+    //                 result = Some(i + 1);
+    //             }
+    //         },
+    //         None => {
+    //             break;
+    //         }
+    //     }
+    // }
 }
