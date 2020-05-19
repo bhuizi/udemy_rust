56556887ffa187f18356124c213e61edbae7bc84
bryan huizi <bryan.huizi@magnet360.com>
2020-05-18 20:21:20 -0700

    hof

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
+fn odd_num(i:i32)-> bool {
 
-// fn sum (a:i32, b:i32) {
-//     let total = a + b;
-//     println!("sum is: {}", total);
-// }
-
-fn max (a:i32, b:i32, c:i32)->i32 {
-    if a > b && a > c {
-        return a;
-    }
-    else if b > a && b > c {
-        return b;
-    }
-    else {
-        return c;
-    }
+    return i % 2 == 1;
 }
+
 
 fn main() {
 
-    // sum(10, 10);
-    let value = max(50, 20, 30);
-    println!("max value is: {}", value);
+    let limit = 1000;
+    let mut sum = 0;
+
+    for i in 0.. {
+        let square = i * i;
+        if square > limit {
+            break;
+        }
+        else if odd_num(square){
+            sum = sum + square;
+        }
+    }
+    println!("find sum off all squared odd numbers under 1000: {}", sum);
+
+    let sum_odd_square=(0..)
+        .map(|n|n*n)
+        .take_while(|&square|square < limit)
+        .filter(|&square|odd_num(square))
+        .fold(0,|sum, square|sum + square);
+
+    println!("find sum off all squared odd numbers under 1000: {}", sum_odd_square);
 }
