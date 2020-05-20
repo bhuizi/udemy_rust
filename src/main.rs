8491d87542ebdd0b0584f8ed1498482edeecedff
bryan huizi <bryan.huizi@magnet360.com>
2020-05-19 20:32:53 -0700

    modules

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-fn diverge()->! {
-    panic!("Problem Occurred");
+
+mod programming {
+
+    pub fn rust(){
+        println!("Welcome to Rust Language");
+    }
+
+    pub fn c_plus_plus() {
+        println!("Welcome to C++ Language");
+    }
+
+    pub mod data_base {
+        pub fn sql() {
+            println!("Welcome to SQL Language");
+        }
+    }
 }
 
 fn main() {
-
-    diverge();
-
-    println!("This line wont be reached");
-
+    programming::rust();
+    programming::c_plus_plus();
+    programming::data_base::sql();
 }
