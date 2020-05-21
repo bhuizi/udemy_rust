360c164bb04e05391a9b2569e041b5a41e072ee0
bryan huizi <bryan.huizi@magnet360.com>
2020-05-20 20:56:45 -0700

    array in fn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
+fn average_age(array:&[i32]) {
+
+    let mut sum = 0;
+
+    for i in 0..10 {
+        sum = sum + array[i];
+    }
+
+    println!("average age of a person is: {}", sum/10);
+
+}
+
+fn sum(array_one:&[i32], array_two:&[i32])->[i32; 5] {
+
+    let mut tmp:[i32; 5] = [0, 0, 0, 0, 0];
+
+    for i in 0..5 {
+        tmp[i] = array_one[i] + array_two[i];
+    }
+    return tmp;
+}
+
 fn main() {
 
-    let array:[i32; 5] = [2, 3, 4, 5, 8];
+    // let age:[i32; 10] = [ 25, 30, 50, 45, 70, 43, 22, 33, 90, 40];
+    let array_one:[i32; 5] = [ 5, 10, 20, 30 , 50];
+    let array_two:[i32; 5] = [10, 20, 30, 35, 40];
 
-    // println!("first value of an array is: {}", array[0]);
+    let array_three:[i32; 5] = sum(&array_one, &array_two);
+
     for i in 0..5 {
-        println!("value of array is: {}", array[i]);
+        println!("value is: {}", array_three[i]);
     }
+    // average_age(&age);
 }
