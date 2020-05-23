29e4a2f455d201c6132d1de2988481bcc28d6e80
bryan huizi <bryan.huizi@magnet360.com>
2020-05-22 20:05:08 -0700

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+fn reverse_order(tup:(i32, f64, bool, i32))-> (i32, bool, f64, i32) {
+    let (a, b, c, d) = tup;
+    (d, c, b, a)
+}
+
 fn main() {
 
-    let arr:[[i32; 4 ];3] = [[1, 2, 3, 4], [4, 5, 6, 7], [8, 9, 10, 11]];
+    let tup:(i32, f64, bool, i32)=( 500, 2.4, true, 30);
 
-    for rows in 0..3 {
-        for cols in 0..4 {
-            println!("value is :{}", arr[rows][cols]);
-        }
-    }
-    // println!("value is: {}", arr[1][3]);
+    let new_tup = reverse_order(tup);
+
+    println!("value is: {}", new_tup.1);
+
+    // let (_a, _b, _c, _d) = tup;
+
+    // println!("value is: {}", _b);
+
+    // println!("value is: {}", tup.1);
+
+    // let tup_2:(i32, f64, bool, i32, (i32, i32, i32)) = (500, 2.4, true, 30, (1, 2, 3));
+    // println!("{}", (tup_2.4).2);
 }
