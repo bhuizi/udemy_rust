c982f2a227b4db5d80018db4fb95dd8d34d8de44
bryan huizi <bryan.huizi@magnet360.com>
2020-05-25 16:36:32 -0700

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
-fn reverse_order(tup:(i32, f64, bool, i32))-> (i32, bool, f64, i32) {
-    let (a, b, c, d) = tup;
-    (d, c, b, a)
-}
-
 fn main() {
 
-    let tup:(i32, f64, bool, i32)=( 500, 2.4, true, 30);
+    // let a = vec![1, 2, 3, 4];
+    // println!("value is: {}", a[2]);
 
-    let new_tup = reverse_order(tup);
+    // for i in a {
+    //     println!("value is: {}", i)
+    // }
 
-    println!("value is: {}", new_tup.1);
+    let mut b = vec![1, 2, 3, 4];
+    b.push(10);
 
-    // let (_a, _b, _c, _d) = tup;
+    for i in &b {
+        println!("value is: {}", i);
+    }
 
-    // println!("value is: {}", _b);
-
-    // println!("value is: {}", tup.1);
-
-    // let tup_2:(i32, f64, bool, i32, (i32, i32, i32)) = (500, 2.4, true, 30, (1, 2, 3));
-    // println!("{}", (tup_2.4).2);
+    println!("length of vector is: {}", b.len());
 }
