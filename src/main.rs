1e94cd716fc5c8a87931d00d8e76108405aceb64
bryan huizi <bryan.huizi@magnet360.com>
2020-05-30 13:55:17 -0700

    closures: FnOnce

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-fn call<F>(closure:F) where F:Fn(){
+fn call<F>(closure:F) where F:FnOnce(){
     closure();
 }
 
 fn main() {
-    let x = 10;
+    let mut x = 10;
 
     let value=|| {
+        x=x+1;
         println!("value of x: {}", x);
     };
     call(value);
