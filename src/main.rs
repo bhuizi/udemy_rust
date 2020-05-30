e39830ea51a08b73b68127f561336c8908a996d5
bryan huizi <bryan.huizi@magnet360.com>
2020-05-30 14:15:56 -0700

    HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-enum Day {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
-}
-
-impl Day {
-    fn is_weekend(&self)->bool {
-        match self {
-            Day::Saturday | Day::Sunday => true,
-            _=> false
-        }
-    }
-}
+use std::collections::HashMap;
 
 fn main() {
-    let d = Day::Sunday;
-    println!("Is it a weekend: {}", d.is_weekend());
+    let mut student=HashMap::new();
+
+    student.insert("A210", "John");
+    student.insert("A211", "Bob");
+    student.insert("A212", "Rock");
+    student.insert("A213", "Kim");
+    student.insert("A214", "Jenny");
+
+    println!("Number of students: {}", student.len());
+
+    match student.get("A210") {
+        Some(value)=>println!("value against key is: {}", value),
+        _=> println!("value not found")
+    }
+
+    student.insert("A214", "Alex");
+
+    for (key, value) in &student {
+        println!("{}:{}", key, value);
+    }
+
+    println!("value contained in hashmap: {}", student.contains_key("A214"));
 }
