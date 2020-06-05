5ee3b1073ee3a00ce68dd8fe9a6acc5a6a6c4021
bryan huizi <bryan.huizi@magnet360.com>
2020-06-05 16:35:10 -0700

    channels multiple senders

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
 
     let (tx,rx) = mpsc::channel();
     thread::spawn( move || {
-        let value = String::from("Hello");
-        tx.send(value).unwrap();
+        let value = vec![
+            String::from("welcome"),
+            String::from("to"),
+            String::from("Rust"),
+            String::from("!!!"),
+        ];
+
+        for i in value {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(2));
+        }
     });
 
-    let received = rx.recv().unwrap();
+    for received in rx {
+        println!("{:?}", received);
+    }
 
-    println!("got the message: {}", received);
 }
